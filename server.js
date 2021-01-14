c25038bbf0cab6a687c50ed25fe0b5cdb1db6d6d
fiddesidde <fredrik.mellberg@gmail.com>
2021-01-14 08:44:41 +0100

    first test

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-var app = express();
+require('dotenv').config();
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+    const pic = req.file;
+    const returnObj = {
+        name: pic.originalname,
+        type: pic.mimetype,
+        size: pic.size,
+    };
 
-
+    res.json(returnObj);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port);
 });
